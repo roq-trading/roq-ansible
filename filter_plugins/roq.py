f946e23bb2ce87313b6118145b032a4e2be653df
thraneh <thraneh@gmail.com>
2023-08-22 11:46:42 +0200

    basic support for fix-bridge configuration

--- filter_plugins/roq.py
+++ filter_plugins/roq.py
@@ -37,6 +37,26 @@ def format_toml_symbols(symbols):
 
     if isinstance(symbols, Mapping):
         return "\n[symbols]\n{}".format(_format_toml_mapping(_helper(symbols)))
+
+    def _helper_fix_bridge_1(value):
+        return "regex={}\n".format(format_toml_simple(value))
+
+    def _helper_fix_bridge_2(key, value):
+        return "[symbols.{}]\n{}\n".format(key, _helper_fix_bridge_1(value))
+
+    def _helper_fix_bridge(value):
+        result = ""
+        for item in value:
+            if isinstance(item, Mapping):
+                for key_2, value_2 in item.items():
+                    result = result + _helper_fix_bridge_2(key_2, value_2)
+            else:
+                result = result + _helper_fix_bridge_1(item)
+        return result
+
+    if isinstance(symbols, list):
+        return "\n[symbols]\n{}".format(_helper_fix_bridge(symbols))
+
     return "\nsymbols = {}".format(format_toml_simple(symbols))
 
 
@@ -54,6 +74,13 @@ def format_toml_users(users):
     return "\n[users]\n{}".format("\n".join(_helper(k, v) for k, v in users.items()))
 
 
+def format_toml_statistics(statistics):
+    def _helper(key, value):
+        return "\n[statistics.{}]\n{}".format(key, _format_toml_mapping(value))
+
+    return "\n[statistics]\n{}".format("\n".join(_helper(k, v) for k, v in statistics.items()))
+
+
 def format_gflags_options(options):
     def _helper(key, value):
         return "--{}={}".format(key, value)
@@ -69,5 +96,6 @@ class FilterModule(object):
             roq_toml_symbols=format_toml_symbols,
             roq_toml_accounts=format_toml_accounts,
             roq_toml_users=format_toml_users,
+            roq_toml_statistics=format_toml_statistics,
             roq_gflags_options=format_gflags_options,
         )
