910a62b6efb51e666e1c40fa1f49328e075af51e
thraneh <thraneh@gmail.com>
2024-12-08 08:11:19 +0100

    simplify

--- filter_plugins/roq.py
+++ filter_plugins/roq.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python
 
-from collections.abc import Mapping, Iterable
-from numbers import Number
-
-from os.path import normpath
-
 import json
 import toml
 
-
-def format_realpath(value):
-    # note! realpath will follow symlinks, e.g. /var => /private/var on OSX
-    return normpath(value).replace("//", "/")
+from collections.abc import Mapping, Iterable
+from numbers import Number
 
 
-def format_toml_simple(value):
+def _format_toml_simple(value):
     if isinstance(value, Mapping):
         assert False, "mapping not allowed"
     elif isinstance(value, bool):
@@ -31,7 +24,7 @@ def format_toml_simple(value):
 
 def _format_toml_mapping(value):
     assert isinstance(value, Mapping), "not a mapping"
-    return "\n".join("{} = {}".format(k, format_toml_simple(v)) for k, v in value.items())
+    return "\n".join("{} = {}".format(k, _format_toml_simple(v)) for k, v in value.items())
 
 
 def format_toml_symbols(symbols):
@@ -42,7 +35,7 @@ def format_toml_symbols(symbols):
         return "\n[symbols]\n{}".format(_format_toml_mapping(_helper(symbols)))
 
     def _helper_fix_bridge_1(value):
-        return "regex={}".format(format_toml_simple(value))
+        return "regex={}".format(_format_toml_simple(value))
 
     def _helper_fix_bridge_2(key, value):
         return "\n\n[symbols.{}]\n{}".format(key, _helper_fix_bridge_1(value))
@@ -64,28 +57,7 @@ def format_toml_symbols(symbols):
         if is_fix_bridge:
             return "\n[symbols]\n{}".format(_helper_fix_bridge(symbols))
 
-    return "\nsymbols = {}".format(format_toml_simple(symbols))
-
-
-def format_toml_accounts(accounts):
-    def _helper(key, value):
-        return "\n[accounts.{}]\n{}".format(key, _format_toml_mapping(value))
-
-    return "\n[accounts]\n{}".format("\n".join(_helper(k, v) for k, v in accounts.items()))
-
-
-def format_toml_users(users):
-    def _helper(key, value):
-        return "\n[users.{}]\n{}".format(key, _format_toml_mapping(value))
-
-    return "\n[users]\n{}".format("\n".join(_helper(k, v) for k, v in users.items()))
-
-
-def format_toml_statistics(statistics):
-    def _helper(key, value):
-        return "\n[statistics.{}]\n{}".format(key, _format_toml_mapping(value))
-
-    return "\n[statistics]\n{}".format("\n".join(_helper(k, v) for k, v in statistics.items()))
+    return "\nsymbols = {}".format(_format_toml_simple(symbols))
 
 
 def format_gflags_options(options):
@@ -107,12 +79,7 @@ def format_toml(value):
 class FilterModule(object):
     def filters(self):
         return dict(
-            roq_realpath=format_realpath,
-            roq_toml_simple=format_toml_simple,
             roq_toml_symbols=format_toml_symbols,
-            roq_toml_accounts=format_toml_accounts,
-            roq_toml_users=format_toml_users,
-            roq_toml_statistics=format_toml_statistics,
             roq_gflags_options=format_gflags_options,
             roq_toml=format_toml,
         )
